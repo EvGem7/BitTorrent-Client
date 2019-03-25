a9f0d14a3fe6be9d0289b4c0ceeeb16fb3b83a5a
EvGem7 <evg200003@gmail.com>
2019-03-25 21:14:49 +0000

    add comments in TorrentInfo

--- app/src/main/java/org/evgem/android/bittorrentclient/data/entity/TorrentInfo.kt
+++ app/src/main/java/org/evgem/android/bittorrentclient/data/entity/TorrentInfo.kt
@@ -3,11 +3,14 @@ package org.evgem.android.bittorrentclient.data.entity
 import java.util.*
 
 data class TorrentInfo(
-    val announces: Set<String>,
+    val announces: Set<String>, //for tracker requests
+
+    //for downloading
     val pieceLength: Int,
     val pieces: List<ByteArray>,
     val files: List<File>,
 
+    //additional information
     val creationDate: Date?,
     val comment: String?,
     val createdBy: String?,
