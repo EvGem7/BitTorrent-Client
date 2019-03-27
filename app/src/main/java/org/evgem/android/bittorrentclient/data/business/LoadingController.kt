b8a8b033fa800d1f02f6c914444ad3cd512f1cf2
EvGem7 <evg200003@gmail.com>
2019-03-27 11:35:01 +0000

    Added LoadingController and PeerAcceptor

---
+++ app/src/main/java/org/evgem/android/bittorrentclient/data/business/LoadingController.kt
@@ -0,0 +1,6 @@
+package org.evgem.android.bittorrentclient.data.business
+
+/**
+ * Controls other controllers: PeerController, TrackerController and PeerAcceptor.
+ */
+class LoadingController
---
+++ app/src/main/java/org/evgem/android/bittorrentclient/data/network/PeerAcceptor.kt
@@ -0,0 +1,6 @@
+package org.evgem.android.bittorrentclient.data.network
+
+/**
+ * Accepts incoming connections from peers
+ */
+class PeerAcceptor
