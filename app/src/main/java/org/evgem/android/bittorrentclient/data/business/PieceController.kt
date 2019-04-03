072a68803264215d03295016522f44f0b33b45a6
EvGem7 <evg200003@gmail.com>
2019-04-03 19:43:35 +0000

    Implemented PieceController

--- app/src/main/java/org/evgem/android/bittorrentclient/data/business/PieceController.kt
+++ app/src/main/java/org/evgem/android/bittorrentclient/data/business/PieceController.kt
@@ -1,12 +1,22 @@
 package org.evgem.android.bittorrentclient.data.business
 
 import org.evgem.android.bittorrentclient.data.entity.TorrentInfo
+import java.io.RandomAccessFile
 import java.lang.IllegalArgumentException
 
 /**
- * Responsible for putting pieces into files.
+ * Responsible for putting pieces into files. Paths to files should be absolute.
  */
 class PieceController(private val torrentInfo: TorrentInfo) {
+    private val files = ArrayList<RandomAccessFile>(torrentInfo.files.size)
+    init {
+        for (file in torrentInfo.files) {
+            val raFile = RandomAccessFile(file.path, "rw")
+            raFile.setLength(file.length)
+            files += raFile
+        }
+    }
+
     val piecesStatus = BooleanArray(torrentInfo.pieces.size)
 
     val uploaded: Long get() = 0 // TODO
@@ -25,7 +35,26 @@ class PieceController(private val torrentInfo: TorrentInfo) {
         if (data.size != torrentInfo.pieceLength) {
             throw IllegalArgumentException("piece size is invalid")
         }
+        if (index >= torrentInfo.pieces.size) {
+            throw IllegalArgumentException("too large index: $index")
+        }
+
+        var offset = index.toLong() * torrentInfo.pieceLength
+        var file: RandomAccessFile? = null
+        for (f in files) {
+            if (offset < f.length()) {
+                file = f
+                break
+            } else {
+                offset -= f.length()
+            }
+        }
+        if (file == null) {
+            throw UnknownError("cannot find file containing this piece. index=$index")
+        }
+        file.seek(offset)
+        file.write(data)
+
         piecesStatus[index] = true
-        TODO("not implemented")
     }
 }
