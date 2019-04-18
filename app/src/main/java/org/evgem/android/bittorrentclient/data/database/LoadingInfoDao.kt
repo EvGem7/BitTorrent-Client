7de37001b89276002b53025ef884674d227841bc
EvGem7 <evg200003@gmail.com>
2019-04-18 15:00:07 +0000

    insert() returns id of new loading. added insertAll()

--- app/src/main/java/org/evgem/android/bittorrentclient/data/database/LoadingInfoDao.kt
+++ app/src/main/java/org/evgem/android/bittorrentclient/data/database/LoadingInfoDao.kt
@@ -2,13 +2,17 @@ package org.evgem.android.bittorrentclient.data.database
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import org.evgem.android.bittorrentclient.data.entity.LoadingInfo
 
 @Dao
 interface LoadingInfoDao {
-    @Insert
-    fun insert(loadingInfo: LoadingInfo)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(loadingInfo: LoadingInfo): Long
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(loadingInfoList: List<LoadingInfo>)
 
     @Query("SELECT * FROM loading")
     fun getAll(): List<LoadingInfo>
